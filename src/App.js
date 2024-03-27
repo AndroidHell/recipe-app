095ee35e1eaac2646d6bcd521852a51f6c7db9f3
AndroidHell <ralkkai1337@gmail.com>
2024-03-27 11:02:56 -0500

    fixed package.json homepage issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  HashRouter,
+} from "react-router-dom";
 import { Home } from "./Home";
 import { Lasagna } from "./pages/Lasagna";
 import { ItalianChickenSoup } from "./pages/ItalianChickenSoup";
@@ -8,7 +13,7 @@ import { PastaWithPeas } from "./pages/PastaWithPeas";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/lasagna" element={<Lasagna />} />
@@ -16,7 +21,7 @@ function App() {
           <Route path="/pastaWithPeas" element={<PastaWithPeas />} />
           <Route path="*" element={<h1>404 PAGE NOT FOUND</h1>} />
         </Routes>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
