8d9495c2e50b11accd504bc6e4cc2c37d6148aa7
AndroidHell <ralkkai1337@gmail.com>
2024-03-27 12:28:35 -0500

    makey worky please

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  HashRouter,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./Home";
 import { Lasagna } from "./pages/Lasagna";
 import { ItalianChickenSoup } from "./pages/ItalianChickenSoup";
@@ -13,7 +8,7 @@ import { PastaWithPeas } from "./pages/PastaWithPeas";
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/lasagna" element={<Lasagna />} />
@@ -21,7 +16,7 @@ function App() {
           <Route path="/pastaWithPeas" element={<PastaWithPeas />} />
           <Route path="*" element={<h1>404 PAGE NOT FOUND</h1>} />
         </Routes>
-      </HashRouter>
+      </Router>
     </div>
   );
 }
